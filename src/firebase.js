4949678e979ed5075e3f4ad5d6ab6eae65bfd3c3
ahlife18 <ahlife18@gmail.com>
2026-08-09 11:31:44 -0400

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 
 // 🔥 This MUST match your NEW project (school-system-live-b2889)
 const firebaseConfig = {
-  apiKey: "AIzaSyANnXIy-yczLOlBF9xQpYYx8Unk_Sco6Y",  // Check this in your new project
+  apiKey: "AIzaSyC6Brw4gzFLwWTnFf5nQCWkltgB_4mJyMU",  // Check this in your new project
   authDomain: "school-system-live-b2889.firebaseapp.com",
   projectId: "school-system-live-b2889",
   storageBucket: "school-system-live-b2889.appspot.com",
