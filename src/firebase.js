2e0abc71bace4d1effe2b34bd029b759fdd0b012
ahlife18 <ahlife18@gmail.com>
2026-08-09 09:54:22 -0400

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnnXly-yczloOlBF9xQpYYx8Unk_Sco6Y",
-  authDomain: "smsp-7948b.firebaseapp.com",
-  projectId: "smsp-7948b",
-  storageBucket: "smsp-7948b.firebasestorage.app",
-  messagingSenderId: "56201683459",
-  appId: "1:56201683459:web:03910a532e2bdb7b36436"
+  apiKey: "AIzaSyC6Brw4gzFLwWTnFf5nQCWkltgB_4mJyMU",
+  authDomain: "school-system-live-b2889.firebaseapp.com",
+  projectId: "school-system-live-b2889",
+  storageBucket: "school-system-live-b2889.firebasestorage.app",
+  messagingSenderId: "554845665554",
+  appId: "1:554845665554:web:7f79b2f12de11ccad05bcb"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
